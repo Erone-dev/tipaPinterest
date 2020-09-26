3625d6768833e2b3d5d468a45b51302a2dcc848e
Erone-dev <ev.pilshakov@mail.ru>
2020-09-26 12:52:54 +0600

    opacity&scale

--- js/main.js
+++ js/main.js
@@ -15,23 +15,23 @@ $(".menu").click(() => {
 
 
 
-var num = 5;
+var num = 2;
 
 function addContent(link, img_link) {
-    var code = "<div class='pin'><a href='" + link + "'><div class='img'><img src='" + img_link + "' alt='img-" + num + "'></div></a></div>"
+    var code = "<div class='pin'><a href='" + link + "'><div class='img'><div><img src='" + img_link + "' alt='img-" + num + "'></div></div></a></div>"
     $(".content-sec").prepend(code);
     num += 1;
 }
 
 addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2F72tv.ru%2Fpictures%2F11905-krasivye-cvety-foto.html&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABAK", "https://72tv.ru/uploads/posts/2018-08/1535284732_dekorativnyy-podsolnuh.jpg");
 addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Ffishki.net%2F2437187-neverojatno-krasivye-fotografii.html&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABAW", "https://cdn.fishki.net/upload/post/2017/11/21/2437187/dwwpkmjf2x8.jpg");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fproprikol.ru%2Fkartinki%2Fskachat-krasivye-kartinki-na-telefon-besplatno-75-foto.html&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABAc", "https://proprikol.ru/wp-content/uploads/2020/02/kachat-krasivye-kartinki-na-telefon-besplatno-1.jpg");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fribalych.ru%2F2017%2F12%2F13%2Fprosto-ochen-krasivye-foto%2F&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABAi", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS3xiFDgrDHxeTUVjzWqTECgYkgk2U0ssTCQw&usqp=CAU");
-addContent("https://www.google.com/url?sa=i&url=http%3A%2F%2Fpikstok.ru%2Fpost%2Fkrasivye-i-neobychnye-kartinki-390576&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABAo", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT6nbO20fNSuafYzzYf5r4FsMR1965WyD0_4Q&usqp=CAU");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fdrasler.ru%2Fkrasivye-kartinki-vertikalnye-hd-podborka%2F&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABAu", "https://drasler.ru/wp-content/uploads/2019/05/%D0%9A%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D0%B2%D0%B5%D1%80%D1%82%D0%B8%D0%BA%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-hd-%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1%80%D0%BA%D0%B0-028.jpg");
+addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/45/300/300");
+addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/78/200/300");
 addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fworldi.ru%2Fsamye-prisamye-krasivye-tsvety-v-mire-10-foto%2F&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABA6", "https://worldi.ru/wp-content/uploads/2019/03/Samye-krasivye-tsvety-v-mire.jpg");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fxn--e1aaajzchnkg.ru.com%2Fporazitelno-krasivye-kartiny-maslom-ot-hudozhnitsy-anastasii-morskoj.html&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABBN", "https://интересное.ru.com/wp-content/uploads/2018/11/Porazitelno-krasivye-kartiny-maslom-ot-hudozhnitsy-Anastasii-Morskoj-11-600x467.jpg");
+addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/98/200/300");
 addContent("https://www.google.com/url?sa=i&url=http%3A%2F%2Fduck.show%2Fkrasivye-foto-odno-mgnovenie.html&psig=AOvVaw1AJY04rxym-uoGa0F6caDJ&ust=1600250204837000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCJ1Nvy6usCFQAAAAAdAAAAABAD","https://duck.show/wp-content/uploads/2019/09/2019-09-17_17-11-17.png");
 addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fklike.net%2F1884-krasivye-kartinki-na-oboi-40-foto.html&psig=AOvVaw1AJY04rxym-uoGa0F6caDJ&ust=1600250204837000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCJ1Nvy6usCFQAAAAAdAAAAABAJ","https://klike.net/uploads/posts/2019-06/1560664251_3.jpg");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Flifeo.ru%2Fkartinki-krasivoj-vesny-na-telefon-skachajte-besplatno%2F&psig=AOvVaw1AJY04rxym-uoGa0F6caDJ&ust=1600250204837000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCJ1Nvy6usCFQAAAAAdAAAAABAP","https://lifeo.ru/wp-content/uploads/vesennaya-kartinka-na-telefon-38.jpg");
+addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/28/300/300");
+addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/17/300/300");
+addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/8/300/300");
 
