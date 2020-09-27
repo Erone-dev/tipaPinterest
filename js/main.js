ef18d2e2f744ca12621a903e698152098739b1c0
Erone-dev <ev.pilshakov@mail.ru>
2020-09-27 13:02:09 +0600

    add imgs

--- js/main.js
+++ js/main.js
@@ -18,24 +18,30 @@ $(".menu").click(() => {
 var num = 0;
 
 function addContent(link, img_link) {
-    var code = "<div class='pin'><a href='" + link + "'><div class='img'><div><img data-src='" + img_link + "' alt='img-" + num + "'></div></div></a></div>"
+    var code = "<div class='pin'><div class='img'><div><a target='_blank' href='" + link + "'><img data-src='" + img_link + "' alt='img-" + num + "'></a></div></div></div>"
     $(".content-sec").prepend(code);
     num += 1;
 }
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/1016/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/200/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/160/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/100/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/96/200/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/201/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/58/200/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/61/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/132/200/300");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fworldi.ru%2Fsamye-prisamye-krasivye-tsvety-v-mire-10-foto%2F&psig=AOvVaw3bOJWloK_O4l7CmAZaMqwT&ust=1600243323225000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDP2YnZ6usCFQAAAAAdAAAAABA6", "https://worldi.ru/wp-content/uploads/2019/03/Samye-krasivye-tsvety-v-mire.jpg");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/98/200/300");
-addContent("https://www.google.com/url?sa=i&url=http%3A%2F%2Fduck.show%2Fkrasivye-foto-odno-mgnovenie.html&psig=AOvVaw1AJY04rxym-uoGa0F6caDJ&ust=1600250204837000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCJ1Nvy6usCFQAAAAAdAAAAABAD","https://duck.show/wp-content/uploads/2019/09/2019-09-17_17-11-17.png");
-addContent("https://www.google.com/url?sa=i&url=https%3A%2F%2Fklike.net%2F1884-krasivye-kartinki-na-oboi-40-foto.html&psig=AOvVaw1AJY04rxym-uoGa0F6caDJ&ust=1600250204837000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCJ1Nvy6usCFQAAAAAdAAAAABAJ","https://klike.net/uploads/posts/2019-06/1560664251_3.jpg");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/28/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/17/300/300");
-addContent("https://picsum.photos/id/237/200/300","https://picsum.photos/id/8/300/300");
+
+var link = "https://picsum.photos/images";
+addContent(link,"https://picsum.photos/id/1016/300/300");
+addContent(link,"https://picsum.photos/id/200/300/300");
+addContent(link,"https://picsum.photos/id/160/300/300");
+addContent(link,"https://picsum.photos/id/100/300/300");
+addContent(link,"https://picsum.photos/id/96/200/300");
+addContent(link,"https://picsum.photos/id/201/300/300");
+addContent(link,"https://picsum.photos/id/58/200/300");
+addContent(link,"https://picsum.photos/id/61/300/300");
+addContent(link,"https://picsum.photos/id/132/200/300");
+addContent(link, "https://picsum.photos/id/1032/300/300");
+addContent(link,"https://picsum.photos/id/98/200/300");
+addContent(link,"https://picsum.photos/id/1015/200/300");
+addContent(link,"https://picsum.photos/id/1020/300/300");
+addContent(link,"https://picsum.photos/id/28/300/300");
+addContent(link,"https://picsum.photos/id/17/300/300");
+addContent(link,"https://picsum.photos/id/8/300/300");
+addContent(link,"https://picsum.photos/id/861/300/300");
+addContent(link,"https://picsum.photos/id/554/300/300");
+addContent(link,"https://picsum.photos/id/789/300/300");
+addContent(link,"https://picsum.photos/id/342/300/300");
 
