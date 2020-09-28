8fd52de1fce86e68e63b8f75324273b638a3811c
Erone-dev <ev.pilshakov@mail.ru>
2020-09-28 15:18:15 +0600

    animation & media

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 'use strict'
 
+var num = 0;
+
+function addContent(link, img_link) {
+    var code = "<div class='pin'><div class='img'><div><a target='_blank' href='" + link + "'><img data-src='" + img_link + "' alt='img-" + num + "'></a></div></div></div>"
+    $(".content-sec").prepend(code);
+    num += 1;
+}
+
+var link = "https://picsum.photos/images";
+addContent(link,"https://picsum.photos/id/1016/350/350");
+addContent(link,"https://picsum.photos/id/666/350/350");
+addContent(link,"https://picsum.photos/id/160/350/350");
+addContent(link,"https://picsum.photos/id/100/350/350");
+addContent(link,"https://picsum.photos/id/96/300/350");
+addContent(link,"https://picsum.photos/id/201/350/350");
+addContent(link,"https://picsum.photos/id/58/300/350");
+addContent(link,"https://picsum.photos/id/61/350/350");
+addContent(link,"https://picsum.photos/id/132/300/350");
+addContent(link, "https://picsum.photos/id/1032/350/350");
+addContent(link,"https://picsum.photos/id/98/300/350");
+addContent(link,"https://picsum.photos/id/1015/300/350");
+addContent(link,"https://picsum.photos/id/1020/350/350");
+addContent(link,"https://picsum.photos/id/28/350/350");
+addContent(link,"https://picsum.photos/id/17/350/350");
+addContent(link,"https://picsum.photos/id/8/350/350");
+addContent(link,"https://picsum.photos/id/861/350/350");
+addContent(link,"https://picsum.photos/id/554/350/350");
+addContent(link,"https://picsum.photos/id/789/350/350");
+addContent(link,"https://picsum.photos/id/342/350/350");
+
 $(".menu").click(() => {
     $(".menu-btn").toggleClass("menu-icon-active");
     $(".menu-sec").toggleClass("open-menu");
@@ -13,35 +43,10 @@ $(".menu").click(() => {
     }
 })
 
+$("#search-inp").focusin(()=>{
+    $(".search").removeClass("anim");
+});
 
-
-var num = 0;
-
-function addContent(link, img_link) {
-    var code = "<div class='pin'><div class='img'><div><a target='_blank' href='" + link + "'><img data-src='" + img_link + "' alt='img-" + num + "'></a></div></div></div>"
-    $(".content-sec").prepend(code);
-    num += 1;
-}
-
-var link = "https://picsum.photos/images";
-addContent(link,"https://picsum.photos/id/1016/300/300");
-addContent(link,"https://picsum.photos/id/200/300/300");
-addContent(link,"https://picsum.photos/id/160/300/300");
-addContent(link,"https://picsum.photos/id/100/300/300");
-addContent(link,"https://picsum.photos/id/96/200/300");
-addContent(link,"https://picsum.photos/id/201/300/300");
-addContent(link,"https://picsum.photos/id/58/200/300");
-addContent(link,"https://picsum.photos/id/61/300/300");
-addContent(link,"https://picsum.photos/id/132/200/300");
-addContent(link, "https://picsum.photos/id/1032/300/300");
-addContent(link,"https://picsum.photos/id/98/200/300");
-addContent(link,"https://picsum.photos/id/1015/200/300");
-addContent(link,"https://picsum.photos/id/1020/300/300");
-addContent(link,"https://picsum.photos/id/28/300/300");
-addContent(link,"https://picsum.photos/id/17/300/300");
-addContent(link,"https://picsum.photos/id/8/300/300");
-addContent(link,"https://picsum.photos/id/861/300/300");
-addContent(link,"https://picsum.photos/id/554/300/300");
-addContent(link,"https://picsum.photos/id/789/300/300");
-addContent(link,"https://picsum.photos/id/342/300/300");
-
+$("#search-inp").focusout(()=>{
+    $(".search").addClass("anim");
+});
